b7c7aa06bb8a612526fcf47d558b9d1f05598289
Nisha bhooshan <bhooshannisha@gmail.com>
2022-04-03 01:06:29 +0530

    my portfolio

--- src/components/NewSection.jsx
+++ src/components/NewSection.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Paper } from "@material-ui/core";
 import { Button } from "@material-ui/core";
 import { Box } from "@material-ui/core";
@@ -7,27 +7,49 @@ import { Grid } from "@material-ui/core";
 import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core";
 import Social from "./Social";
+import Hidden from "@material-ui/core/Hidden";
+import Zoom from "@material-ui/core/Zoom";
+import { StaticImage } from "gatsby-plugin-image";
 
 const useStyles = makeStyles((theme) => ({
   section: {
     height: "90vh",
-    backgroundImage:
-      "url(https://images.unsplash.com/photo-1545665277-5937489579f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80)",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
   },
   content: {
     height: "100%",
   },
   container: {
     height: "100%",
+    zIndex: 100,
+    position: "relative",
+  },
+  overlay: {
+    backgroundColor: "rgba(0,0,0,0.4)",
+    height: "100%",
+    width: "100%",
+    position: "absolute",
+    zIndex: 2,
+  },
+  newImage: {
+    height: "100%",
+    width: "100%",
+    zIndex: 1,
   },
 }));
 
 export default function NewSection() {
   const styles = useStyles();
+  const [shouldShow, setShouldShow] = useState(false);
+  useEffect(() => setShouldShow(true));
   return (
     <Paper className={styles.section}>
+      <StaticImage
+        className={styles.newImage}
+        src="https://images.unsplash.com/photo-1545665277-5937489579f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+        alt=""
+        style={{ position: "absolute" }}
+      />
+      <div className={styles.overlay}></div>
       <Container className={styles.container} maxWidth="md">
         <Grid
           className={styles.content}
@@ -35,26 +57,31 @@ export default function NewSection() {
           justifyContent="space-between"
           alignItems="center"
         >
-          <Grid item sm={8}>
-            <Typography component="h1" variant="h3">
-              Hi, I'm Nisha Bhooshan. I'm a software engineer.{" "}
-            </Typography>
-            <Typography variant="h5">
-              I build websites, web application and responsive user interface.
-            </Typography>
-            <Box my={2}>
-              <Button
-                href="mailto:address@gmail.com"
-                variant="outlined"
-                color="secondary"
-              >
-                Get in touch{" "}
-              </Button>
-            </Box>
-          </Grid>
-          <Grid item>
-            <Social />{" "}
-          </Grid>
+          <Zoom in={shouldShow}>
+            <Grid item sm={8}>
+              <Typography component="h1" variant="h3">
+                Hi, I'm Nisha Bhooshan. I'm a software engineer.{" "}
+              </Typography>
+              <Typography variant="h5">
+                I build websites, web application and responsive user interface.
+              </Typography>
+              <Box my={2}>
+                <Button
+                  href="mailto:address@gmail.com"
+                  variant="outlined"
+                  color="secondary"
+                >
+                  Get in touch{" "}
+                </Button>
+              </Box>
+            </Grid>
+          </Zoom>
+
+          <Hidden xsDown>
+            <Grid item>
+              <Social direction="column" />
+            </Grid>
+          </Hidden>
         </Grid>
       </Container>
     </Paper>
--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import { GithubIcon } from '@material-ui/icons/GitHub'
-import { TwitterIcon } from '@material-ui/icons/Twitter'
-import { LinkedInIcon } from '@material-ui/icons/LinkedIn'
+import React from "react";
+import GithubIcon from "@material-ui/icons/GitHub";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import  Link from "@material-ui/core/Link";
 
 const socialItems = [
-    {icon:GithubIcon,url:"https://github.com/Nishabhooshan"},
-    {icon:TwitterIcon,url:"https://twitter.com/"},
-    {icon:LinkedInIcon,url:"https://www.linkedin.com/in/nisha-bhooshan/"}
-]
+  { icon: GithubIcon, url: "https://github.com/Nishabhooshan" },
+  { icon: TwitterIcon, url: "https://twitter.com/" },
+  { icon: LinkedInIcon, url: "https://www.linkedin.com/in/nisha-bhooshan/" },
+];
 
-export default function Social() {
+export default function Social({direction}) {
   return (
-    <div>
-         {socialItems.map((item) => <item.icon/>) }
-    </div>
-  )
+    <Grid container direction={direction || "row"} spacing={1}>
+      {socialItems.map((item) => (
+        <Grid item>
+            <Link href={item.url}>
+          <IconButton>
+            <item.icon />{" "}
+          </IconButton>
+          </Link>
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
